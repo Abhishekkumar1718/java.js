2008cdb646339207713d3ec01a525ae7b7ab2588
Abhishekkumar1718 <abhishekk89739@gmail.com>
2025-05-24 14:03:12 +0000

    variable

---
+++ basic/variable.js
@@ -0,0 +1,18 @@
+const accountId = 1234
+let emailId ="abhishek@gmail.com"
+var accountPassword ="1234"
+accountCity ="jaipur"
+let accountstate
+/*
+note 
+perfer not use var
+because issuse in block scope and functional scope
+*/
+
+emailId ="abh@gmai.com"
+accountPassword = "2589"
+accountCity = "bhopal"
+
+
+console.log(accountId)
+console.table([emailId,accountPassword,accountCity,accountstate])
