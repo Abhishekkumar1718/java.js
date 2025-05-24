40855ffda5b05473b5e1ca80877e7f347d9d3b5a
Abhishekkumar1718 <abhishekk89739@gmail.com>
2025-05-24 14:02:46 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict" //treat all js code as new version
+
+console.log(3+3)//code readablity should be high
+console.log ("abhishek")
+
+let name = "abhishek"
+let age = 18
+let islogedin = false
+
+//number =>2 power 53
+// bigint
+//string =>""
+//boolean=>true false
+//null => standalone value
+//undefined =>
+//symbole => unique
+
+//object
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
